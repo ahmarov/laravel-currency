9500ffcebf170aa08f8cbda3c786422dd4514806
Dmitri Ahmarov <ahmarov@gmail.com>
2018-05-18 09:13:29 +0300

    Laravel 5.4 support added

--- src/Torann/Currency/CurrencyServiceProvider.php
+++ src/Torann/Currency/CurrencyServiceProvider.php
@@ -6,92 +6,92 @@ use Illuminate\Support\ServiceProvider;
 
 class CurrencyServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        if ($this->isLumen() === false) {
-            $this->publishes([
-                __DIR__ . '/../../config/currency.php' => config_path('currency.php'),
-            ]);
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		if ($this->isLumen() === false) {
+			$this->publishes([
+				__DIR__ . '/../../config/currency.php' => config_path('currency.php'),
+			]);
 
-            $this->mergeConfigFrom(
-                __DIR__ . '/../../config/currency.php', 'currency'
-            );
-        }
+			$this->mergeConfigFrom(
+				__DIR__ . '/../../config/currency.php', 'currency'
+			);
+		}
 
-        $this->publishes([
-            __DIR__.'/../../migrations' => base_path('/database/migrations'),
-        ], 'migrations');
-    }
+		$this->publishes([
+			__DIR__.'/../../migrations' => base_path('/database/migrations'),
+		], 'migrations');
+	}
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerCurrency();
-        $this->registerCurrencyCommands();
-    }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerCurrency();
+		$this->registerCurrencyCommands();
+	}
 
-    /**
-     * Register currency provider.
-     *
-     * @return void
-     */
-    public function registerCurrency()
-    {
-        $this->app->bind('currency', function ($app) {
-            $config = $app->config->get('currency', []);
-            return new Currency($config, $app['cache']);
-        });
-    }
+	/**
+	 * Register currency provider.
+	 *
+	 * @return void
+	 */
+	public function registerCurrency()
+	{
+		$this->app->bind('currency', function ($app) {
+			$config = $app->config->get('currency', []);
+			return new Currency($config, $app['cache']);
+		});
+	}
 
-    /**
-     * Register generator of Currency.
-     *
-     * @return void
-     */
-    public function registerCurrencyCommands()
-    {
-        $this->app['currency.update'] = $this->app->share(function ($app) {
-            return new Commands\CurrencyUpdateCommand($app['currency']);
-        });
+	/**
+	 * Register generator of Currency.
+	 *
+	 * @return void
+	 */
+	public function registerCurrencyCommands()
+	{
+		$this->app->singleton('currency.update', function ($app) {
+			return new Commands\CurrencyUpdateCommand($app['currency']);
+		});
 
-        $this->app['currency.cleanup'] = $this->app->share(function ($app) {
-            return new Commands\CurrencyCleanupCommand($app['currency']);
-        });
+		$this->app->singleton('currency.cleanup', function ($app) {
+			return new Commands\CurrencyCleanupCommand($app['currency']);
+		});
 
-        $this->commands(
-            'currency.update',
-            'currency.cleanup'
-        );
-    }
+		$this->commands(
+			'currency.update',
+			'currency.cleanup'
+		);
+	}
 
-    /**
-     * Check if package is running under Lumen app
-     *
-     * @return bool
-     */
-    protected function isLumen()
-    {
-        return str_contains($this->app->version(), 'Lumen') === true;
-    }
+	/**
+	 * Check if package is running under Lumen app
+	 *
+	 * @return bool
+	 */
+	protected function isLumen()
+	{
+		return str_contains($this->app->version(), 'Lumen') === true;
+	}
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [
-            'currency',
-        ];
-    }
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [
+			'currency',
+		];
+	}
 }
--- src/Torann/Currency/Middleware/CurrencyMiddleware.php
+++ src/Torann/Currency/Middleware/CurrencyMiddleware.php
@@ -7,59 +7,59 @@ use Illuminate\Http\Request;
 
 class CurrencyMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Request $request
-     * @param  Closure $next
-     *
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        // Don't redirect the console
-        if ($this->runningInConsole()) {
-            return $next($request);
-        }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  Request $request
+	 * @param  Closure $next
+	 *
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		// Don't redirect the console
+		if ($this->runningInConsole()) {
+			return $next($request);
+		}
 
-        // Check for a user defined currency
-        if ($request->get('currency') && currency()->hasCurrency($request->get('currency'))) {
-            $currency = $request->get('currency');
-        }
-        elseif ($request->getSession()->get('currency')
-            && currency()->hasCurrency($request->getSession()->get('currency'))
-        ) {
-            $currency = $request->getSession()->get('currency');
-        }
-        else {
-            $currency = currency()->getConfig('default');
-        }
+		// Check for a user defined currency
+		if ($request->get('currency') && currency()->hasCurrency($request->get('currency'))) {
+			$currency = $request->get('currency');
+		}
+		elseif ($request->getSession()->get('currency')
+			&& currency()->hasCurrency($request->getSession()->get('currency'))
+		) {
+			$currency = $request->getSession()->get('currency');
+		}
+		else {
+			$currency = currency()->getConfig('default');
+		}
 
-        // Set user currency
-        $this->setUserCurrency($currency, $request);
+		// Set user currency
+		$this->setUserCurrency($currency, $request);
 
-        return $next($request);
-    }
+		return $next($request);
+	}
 
-    /**
-     * Determine if the application is running in the console.
-     *
-     * @return bool
-     */
-    protected function runningInConsole()
-    {
-        return app()->runningInConsole();
-    }
+	/**
+	 * Determine if the application is running in the console.
+	 *
+	 * @return bool
+	 */
+	protected function runningInConsole()
+	{
+		return app()->runningInConsole();
+	}
 
-    /**
-     * Set the user currency.
-     *
-     * @param string  $currency
-     * @param Request $request
-     */
-    protected function setUserCurrency($currency, $request)
-    {
-        currency()->setCurrency($currency);
-        $request->getSession()->set('currency', $currency);
-    }
+	/**
+	 * Set the user currency.
+	 *
+	 * @param string  $currency
+	 * @param Request $request
+	 */
+	protected function setUserCurrency($currency, $request)
+	{
+		currency()->setCurrency($currency);
+		$request->getSession()->put('currency', $currency);
+	}
 }
